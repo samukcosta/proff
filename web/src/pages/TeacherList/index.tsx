4077fb24a3803ba3b8a0d858e21913218b5038c9
Samuel Costa <samu.ks@outlook.com>
2022-07-16 00:57:10 -0300

    stylization of teacher cards

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
 
@@ -26,6 +27,35 @@ function TeacherList() {
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars.githubusercontent.com/u/12562944?v=4" alt="Samuel Costa" />
+                        <div>
+                            <strong>Samuel Costa</strong>
+                            <span>Fisica</span>
+                        </div>
+                    </header>
+
+                    <p>
+                         Entusiasta das melhores tecnologias que a física pode proporcionar.
+                         <br/>
+                         Apaixonado por fazer experiências malucas no laboratório (e fora dele também!), sempre colocando a vida das pessoas em risco.
+                    </p>
+
+                    <footer>
+                        <p>
+                            Preço/hora
+                            <strong>R$ 80,00</strong>
+                        </p>
+                        <button type='button'>
+                            <img src={whatsappIcon} alt="Whatsapp" />
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
     )
 }
